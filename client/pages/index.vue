304b5a08938b43bb4fb8a2ef3d2e49d12c53b683
yehan <1513849675@qq.com>
2024-04-11 08:32:49 +0800

    🐳 chore: change background

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -13,17 +13,13 @@
 
 <script setup lang="ts">
 // import Dock from "primevue/dock";
+import Wallpapers from "@/utils/wallpapers";
 import { useThemeStore } from "@/store/theme";
 const themeStore = useThemeStore();
 
-// Nuxt 不加前缀默认取public下
-const wallpapers = {
-  day: "img/ui/wallpaper-day.jpg",
-  night: "img/ui/wallpaper-night.jpg",
-};
-
 const backgroundStyle = computed(() => {
-  const imageUrl = themeStore.dark ? wallpapers.night : wallpapers.day;
+  // const imageUrl = themeStore.dark ? Wallpapers.NIGHT : Wallpapers.DAY;
+  const imageUrl = Wallpapers.GITHUB;
   const filterValue = `brightness(${themeStore.brightness * 0.7 + 50}%)`;
   return {
     backgroundImage: `url(${imageUrl})`,
---
+++ client/utils/wallpapers.ts
@@ -0,0 +1,9 @@
+// Nuxt 不加前缀默认取public下
+enum Wallpapers {
+  DAY = "img/ui/wallpaper-day.jpg",
+  NIGHT = "img/ui/wallpaper-night.jpg",
+  GITHUB = "img/ui/github.jpg",
+  VALLEY = "img/ui/valley.jpeg",
+}
+
+export default Wallpapers;
